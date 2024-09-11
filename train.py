79b2d55245b33de1c720c246b8bad1ef84bf4fd7
Chaima Nemir <100592889+nemir-chaima@users.noreply.github.com>
2024-09-11 08:34:19 +0100

    Update train.py

--- train.py
+++ train.py
@@ -15,7 +15,8 @@ def data_split(data):
     y = data['Churn']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     return X_train , X_test, y_train, y_test
-    #
+
+
 
 def normalisation(X_train , X_test):
     MN = MinMaxScaler()
